4abf5e55bcda42664dbcc007970a88efa6525fb2
gaurangrshah <gaurang.rshah@gmail.com>
2024-08-26 19:44:43 -0400

    updates prettier config

---
+++ .prettier.mjs
@@ -0,0 +1,13 @@
+// @SEE: https://docs.astro.build/en/editor-setup/#prettier
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
