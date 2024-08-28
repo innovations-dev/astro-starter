a3b48f5026875539ae5788c7e46473db60d9a1ef
innov8 <greg.shah@momentumwealthpartners.com>
2024-08-28 10:45:32 -0400

    remove prettier adds card dropdown accordion + fixes carousel

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -2,6 +2,22 @@ const eslintPluginAstro = require("eslint-plugin-astro");
 
 module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
+      },
+    },
+  ],
   ...eslintPluginAstro.configs["flat/recommended"],
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   env: {
