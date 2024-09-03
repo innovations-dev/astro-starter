2b9b866e63ba99b5077a8bf3564518bfec022108
innov8 <greg.shah@momentumwealthpartners.com>
2024-09-03 00:06:24 -0400

    final: astro, eslint / prettier / tailwind css config updates

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,29 +1,37 @@
 const eslintPluginAstro = require("eslint-plugin-astro");
 
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "plugin:astro/recommended",
+    "plugin:jsx-a11y/strict",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
     sourceType: "module",
-    ecmaVersion: "latest",
+    ecmaVersion: "latest"
   },
   overrides: [
     {
       files: ["*.astro"],
       parser: "astro-eslint-parser",
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro']
+        ecmaVersion: "latest",
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+        project: "./tsconfig.json"
       },
-    },
+      rules: {}
+    }
   ],
   ...eslintPluginAstro.configs["flat/recommended"],
   ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   env: {
-    node: true,
+    node: true
   },
   rules: {
-    "@typescript-eslint/ban-ts-comment": "warn",
-  },
+    "@typescript-eslint/ban-ts-comment": "warn"
+  }
 };
