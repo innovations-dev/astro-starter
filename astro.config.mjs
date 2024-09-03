720348459c4f5eb48fb3d24ae5ce64699cd8bdac
innov8 <greg.shah@momentumwealthpartners.com>
2024-09-03 19:16:52 -0400

    configure robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,46 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import icon from "astro-icon";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  integrations: [tailwind(), sitemap(), icon()],
+  output: "hybrid",
+  integrations: [
+    tailwind(),
+    sitemap(),
+    icon(),
+    robotsTxt({
+      sitemap: true,
+      sitemapBaseFileName: "sitemap-index",
+      host: "https://momentuminnovations.com",
+      policy: [
+        {
+          userAgent: "*",
+          allow: "/",
+          disallow: [
+            "/admin",
+            "/api",
+            "/private",
+            "/dashboard",
+            "/login",
+            "/logout",
+            "/signup",
+            "/reset-password",
+            "/forgot-password",
+            "/404"
+          ],
+          crawlDelay: 2
+        }
+      ]
+    })
+  ],
   vite: {
     ssr: {
-      noExternal: ['embla-carousel'],
-    },
-  },
+      noExternal: ["embla-carousel"]
+    }
+  }
 });
